52d04c39152755db17ab628f85e8122eac9518ca
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-17 16:46:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+a = [10, 20, 30, 40, 50]
+b = [3, 5, 7, 9, 11]
+natija = list(map(lambda x, y: x - y, a, b))
+print(natija)
+
+oldin = [85, 92, 78, 88, 80]
+keyin = [90, 88, 82, 95, 85]
+natija = list(map(lambda x, y: x - y, oldin, keyin))
+print(natija)
+
+kun = [25, 28, 30, 22, 27]
+tun = [15, 18, 20, 12, 16]
+natija = list(map(lambda x, y: x - y, kun, tun))
+print(natija)
